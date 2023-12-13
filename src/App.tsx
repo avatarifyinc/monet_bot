81724bab27d1dc2bb7dfa20c547b49f1cadacd75
iower <iower@tutanota.com>
2023-12-13 22:59:15 +0500

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>monet_bot webapp</h1>
+      {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+      {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
+    </>
+  )
+}
+
+export default App
