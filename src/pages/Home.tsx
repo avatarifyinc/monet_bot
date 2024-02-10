75d0bc60725acd84a0eb4a2ffcedc40a2b61336a
iower <iower@tutanota.com>
2024-02-10 21:13:15 +0500

    Home: clickable items

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useWebApp } from '@vkruglikov/react-telegram-web-app';
+
+import { useNavigate } from 'react-router-dom'
+
 import Header from '../kit/Header'
 import Screen from '../kit/Screen'
 import New from '../kit/New'
@@ -8,15 +12,19 @@ import editUpscale from '../assets/edit-upscale.jpg'
 import editEraser from '../assets/edit-eraser.jpg'
 import editUncrop from '../assets/edit-uncrop.jpg'
 
+const edits = [
+  { title: 'Add&Replace', img: editAddreplace, path: '/add-replace' },
+  { title: 'AI Outfits', img: editOutfits, path: '/outfits', isNew: true },
+  { title: 'AI Upscale', img: editUpscale, botCommand: "SELECTED_UPSCALE" },
+  { title: 'Eraser', img: editEraser, path: '/eraser' },
+  { title: 'Uncrop', img: editUncrop, path: '/uncrop' },
+  { title: 'Generate image', botCommand: "SELECTED_GENERATE" },
+]
+
 function Home() {
-  const edits = [
-    { title: 'Add&Replace', img: editAddreplace, path: '/add-replace' },
-    { title: 'AI Outfits', img: editOutfits, path: '/outfits', isNew: true },
-    { title: 'AI Upscale', img: editUpscale, botCommand: "UPSCALE" },
-    { title: 'Eraser', img: editEraser, path: '/eraser' },
-    { title: 'Uncrop', img: editUncrop, path: '/uncrop' },
-    { title: 'Generate image', botCommand: "GENERATE" },
-  ]
+  const WebApp = useWebApp()
+  const navigate = useNavigate()
+
   return (
     <Screen>
       <Header onBack={() => { history.back() }} />
@@ -25,12 +33,24 @@ function Home() {
         <h4 className="">AI Edits</h4>
         <div className="mt-6 grid grid-cols-2 gap-3">
           {edits.map(item => (
-            <div className="relative bg-oslo/[0.08] rounded-[12px] pb-[109%] bg-cover bg-center overflow-hidden" style={{ backgroundImage: `url(${item.img})` }}>
+            <button
+              className="relative bg-oslo/[0.08] rounded-[12px] pb-[109%] bg-cover bg-center overflow-hidden"
+              style={{ backgroundImage: `url(${item.img})`}}
+              onClick={() => {
+                if (item.botCommand) {
+                  WebApp.sendData(item.botCommand)
+                  WebApp.close()
+                }
+                if (item.path) {
+                  navigate(item.path)
+                }
+              }}
+            >
               {item.isNew && (
                 <New />
               )}
               <div className="absolute bottom-0 w-full p-3 pt-5 text-[15px] leading-5 font-semibold text-white bg-gradient-to-b from-transparent to-oslo">{item.title}</div>
-            </div>
+            </button>
           ))}
         </div>
       </div>
