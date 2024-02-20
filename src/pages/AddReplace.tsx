612addff8a64befe748de7519aa97fa6574f7ef1
iower <iower@tutanota.com>
2024-02-21 02:21:50 +0500

    Editor: zoom draft

--- src/pages/AddReplace.tsx
+++ src/pages/AddReplace.tsx
@@ -1,9 +1,11 @@
 import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { useState, useRef } from 'react'
+import { useState, useRef, useMemo, useCallback, useEffect } from 'react'
 import { ReactSketchCanvas, type ReactSketchCanvasRef } from 'react-sketch-canvas'
 
 import { BottomSheet } from 'react-spring-bottom-sheet'
 import 'react-spring-bottom-sheet/dist/style.css'
+
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 import { useInit } from '../hooks'
 
@@ -63,22 +65,129 @@ function AddReplace({ mode } : { mode?: 'ERASE'}) {
       })
   }
 
+  const src = testPhoto;
+  const scaleUp = true;
+  const zoomFactor = 8;
+
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  const [containerWidth, setContainerWidth] = useState<number>(0);
+  const [containerHeight, setContainerHeight] = useState<number>(0);
+
+  const [imageNaturalWidth, setImageNaturalWidth] = useState<number>(0);
+  const [imageNaturalHeight, setImageNaturalHeight] = useState<number>(0);
+
+  const imageScale = useMemo(() => {
+    if (
+      containerWidth === 0 ||
+      containerHeight === 0 ||
+      imageNaturalWidth === 0 ||
+      imageNaturalHeight === 0
+    )
+      return 0;
+    const scale = Math.min(
+      containerWidth / imageNaturalWidth,
+      containerHeight / imageNaturalHeight,
+    );
+    return scaleUp ? scale : Math.max(scale, 1);
+  }, [
+    scaleUp,
+    containerWidth,
+    containerHeight,
+    imageNaturalWidth,
+    imageNaturalHeight,
+  ]);
+
+  const handleResize = useCallback(() => {
+    if (container !== null) {
+      const rect = container.getBoundingClientRect();
+      setContainerWidth(rect.width);
+      setContainerHeight(rect.height);
+    } else {
+      setContainerWidth(0);
+      setContainerHeight(0);
+    }
+  }, [container]);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [handleResize]);
+
+  const handleImageOnLoad = (image: HTMLImageElement) => {
+    setImageNaturalWidth(image.naturalWidth);
+    setImageNaturalHeight(image.naturalHeight);
+  };
+
+  useEffect(() => {
+    const image = new Image();
+    image.onload = () => handleImageOnLoad(image);
+    image.src = src;
+  }, [src]);
+
+  const scaleFactor = 1
+
   return (
     <Screen isBottomButton className="flex flex-col !pt-0">
       <Header onBack={() => { history.back() }} />
 
-      <div className="flex-1 bg-[#8881] relative select-none">
-        <img className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[100%] max-h-[100%]" src={testPhoto} />
-        <ReactSketchCanvas
-          ref={canvasRef}
-          // width="100%"
-          // height="150px"
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[100%] max-h-[100%] text-[#9e3e6caa]"
-          canvasColor="transparent"
-          strokeWidth={strokeWidth}
-          eraserWidth={strokeWidth}
-          strokeColor="currentColor"
-        />
+      <div className="flex-1 relative select-none">
+        <div
+          className="absolute w-[100%] h-[100%] bg-[#8881]"
+          ref={(el: HTMLDivElement | null) => setContainer(el)}
+        >
+          {imageScale > 0 && (
+            <TransformWrapper
+              initialScale={scaleFactor || imageScale}
+              minScale={scaleFactor || imageScale}
+              maxScale={(scaleFactor || imageScale) * zoomFactor}
+              centerOnInit
+
+              doubleClick={{ excluded: [], step: 0.7 }}
+              onInit={function noRefCheck(){}}
+              onPanning={function noRefCheck(){}}
+              onPanningStart={function noRefCheck(){}}
+              onPanningStop={function noRefCheck(){}}
+              onPinching={function noRefCheck(){}}
+              onPinchingStart={function noRefCheck(){}}
+              onPinchingStop={function noRefCheck(){}}
+              onTransformed={function noRefCheck(){}}
+              onWheel={function noRefCheck(){}}
+              onWheelStart={function noRefCheck(){}}
+              onWheelStop={function noRefCheck(){}}
+              onZoom={function noRefCheck(){}}
+              onZoomStart={function noRefCheck(){}}
+              onZoomStop={function noRefCheck(){}}
+              panning={{ activationKeys: [], excluded: [] }}
+              pinch={{ excluded: [], step: 5 }}
+              wheel={{ activationKeys: [], excluded: [], smoothStep: 0.001, step: 0.2 }}
+            >
+              <TransformComponent
+                wrapperStyle={{ width: '100%', height: '100%' }}
+              >
+                <div className="EditorFieldWrapper inline relative ?outline-dotted">
+                  <img
+                    className="block"
+                    src={src}
+                  />
+                  <ReactSketchCanvas
+                    ref={canvasRef}
+                    // width="100%"
+                    // height="150px"
+                    className="EditorField absolute left-0 top-0 w-full h-full border-none ?border-red-600 !rounded-none text-[#9e3e6caa]"
+                    canvasColor="transparent"
+                    strokeWidth={strokeWidth}
+                    eraserWidth={strokeWidth}
+                    strokeColor="currentColor"
+                  />
+                </div>
+              </TransformComponent>
+            </TransformWrapper>
+          )}
+        </div>
       </div>
 
       <div className="">
